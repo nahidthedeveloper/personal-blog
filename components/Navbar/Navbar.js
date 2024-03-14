6ea3df4eb0fee7e64c87a7a77bf5c0fca3ce179f
nahid-the-developer <nahidcse.pub@gmail.com>
2024-03-15 02:01:25 +0600

    fix navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     ]
 
     return (
-        <div>
+        <div className='sticky top-0 bg-white dark:bg-gray-900 py-2'>
             <button className={'md:hidden absolute right-6 top-10'} onClick={() => setToggle(!toggle)}>
                 {!toggle ? <Bars3Icon className={'h-8 w-8'} /> : ''}
             </button>
